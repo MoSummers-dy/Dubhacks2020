e9edfdcd85a29e0bacdf7e2e75585d0047b0e764
Diana Dai <dianadai.dy@gmail.com>
2020-10-17 16:53:49 -0700

    minor edit on medicines

--- src/components/medicine/TodoStats.js
+++ src/components/medicine/TodoStats.js
@@ -18,6 +18,7 @@ const TodoStats = (props) => {
       <p className="stats"><span>{props.list.length}</span> Medicines</p>
       <p className="stats"><span>{props.finished}</span> Have Taken</p>
       <p className="stats-percent"><span>{props.percent}%</span> Done</p>
+      {congrats(props.percent)}
     </div>
   );
 };
