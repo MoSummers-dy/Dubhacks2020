e71a48bf363b9682770c86328265848dcf74a5bf
Sean Yang <seanyang@Seans-MacBook-Pro.local>
2020-10-17 21:09:32 -0700

    add dynamic edit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,21 +10,64 @@ class Profile extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-				name: null,
-				age: null,
-				birthday: null,
-				symptoms: null
+			name: "Harry the husky",
+			preferredName: "Harry",
+			birthday: "2000-1-1",
+			phone: "123-456-7890",
+			address: "472 1st Ave N",
+			symptoms: "I can't sleep at night"
 		}
-		this.fetchData()
+
 	}
 
-	fetchData() {
-		// support static data only at phase 1
-		this.state.name = "Husky"
-		this.state.age = 20
-		this.state.birthday = "2000-1-1"
-		this.state.symptoms = "Insomnia"
-	}
+	changeName = (event) => {
+		let name = prompt("Please enter new name")
+		if (name == null || name == "") return
+    this.setState({
+			name: name
+    });
+	};
+	
+	changePreferredName = (event) => {
+		let name = prompt("Please enter new preferred name")
+		if (name == null || name == "") return
+    this.setState({
+			preferredName: name
+    });
+	};
+
+	changeBirthday = (event) => {
+		let day = prompt("Please enter new birthday")
+		if (day == null || day == "") return
+    this.setState({
+			birthday: day
+    });
+	};
+
+	changePhone = (event) => {
+		let phone = prompt("Please enter new name")
+		if (phone == null || phone == "") return
+    this.setState({
+			phone: phone
+    });
+	};
+
+	changeAddress = (event) => {
+		let adr = prompt("Please enter new name")
+		if (adr == null || adr == "") return
+    this.setState({
+			address: adr
+    });
+	};
+
+	changeSymptoms = (event) => {
+		let description = prompt("Please enter new name")
+		if (description == null || description == "") return
+    this.setState({
+			symptoms: description
+    });
+	};
+
 
 	render() {
 		return (
@@ -38,27 +81,27 @@ class Profile extends Component {
 							<Card.Body>
 								<Card.Title>Name</Card.Title>
 								<Card.Text>
-									Harry the Husky
+									{this.state.name}
 								</Card.Text>
-								<Card.Link href="#">Change name</Card.Link>
+								<Card.Link onClick={this.changeName}>Change name</Card.Link>
 							</Card.Body>
 						</Card>
 						<Card style={{ width: '18rem' }}>
 							<Card.Body>
 								<Card.Title>Preferred name</Card.Title>
 								<Card.Text>
-									Harry
+									{this.state.preferredName}
 								</Card.Text>
-								<Card.Link href="#">Change nickname</Card.Link>
+								<Card.Link onClick={this.changePreferredName}>Change nickname</Card.Link>
 							</Card.Body>
 						</Card>
 						<Card style={{ width: '18rem' }}>
 							<Card.Body>
 								<Card.Title>Birthday</Card.Title>
 								<Card.Text>
-									2000-1-1
+									{this.state.birthday}
 								</Card.Text>
-								<Card.Link href="#">Change birthday</Card.Link>
+								<Card.Link onClick={this.changeBirthday}>Change birthday</Card.Link>
 							</Card.Body>
 						</Card>
 					</div>
@@ -70,62 +113,31 @@ class Profile extends Component {
 							<Card.Body>
 								<Card.Title>Phone number</Card.Title>
 								<Card.Text>
-									123-456-7890
+									{this.state.phone}
 								</Card.Text>
-								<Card.Link href="#">Change phone number</Card.Link>
+								<Card.Link onClick={this.changePhone}>Change phone number</Card.Link>
 							</Card.Body>
 						</Card>
 						<Card style={{ width: '18rem' }}>
 							<Card.Body>
 								<Card.Title>Address</Card.Title>
 								<Card.Text>
-									123 1st Street
+									{this.state.address}
 								</Card.Text>
-								<Card.Link href="#">Change address</Card.Link>
+								<Card.Link onClick={this.changeAddress}>Change address</Card.Link>
 							</Card.Body>
 						</Card>
 						<Card style={{ width: '18rem' }}>
 							<Card.Body>
 								<Card.Title>Symptoms</Card.Title>
 								<Card.Text>
-									I can't sleep at night
+									{this.state.symptoms}
 								</Card.Text>
-								<Card.Link href="#">Change symptoms</Card.Link>
+								<Card.Link onClick={this.changeSymptoms}>Change symptoms</Card.Link>
 							</Card.Body>
 						</Card>
 					</div>
-					<div class="row justify-content-between">
-						<h1>  </h1>
-					</div>
-					<div class="row justify-content-between" style={{marginTop: '10pt'}}>
-						<Card style={{ width: '18rem' }}>
-							<Card.Body>
-								<Card.Title>Add new section</Card.Title>
-								<Card.Text>
-									Add new description
-								</Card.Text>
-								<Card.Link href="#">Change information</Card.Link>
-							</Card.Body>
-						</Card>
-						<Card style={{ width: '18rem' }}>
-							<Card.Body>
-								<Card.Title>Add new section</Card.Title>
-								<Card.Text>
-									Add new description
-								</Card.Text>
-								<Card.Link href="#">Change information</Card.Link>
-							</Card.Body>
-						</Card>
-						<Card style={{ width: '18rem' }}>
-							<Card.Body>
-								<Card.Title>Add new section</Card.Title>
-								<Card.Text>
-									Add new description
-								</Card.Text>
-								<Card.Link href="#">Change information</Card.Link>
-							</Card.Body>
-						</Card>
-					</div>
+					
 				</Container>
 				<div class="row justify-content-between" style={{marginTop: '10pt'}}>
 				</div>
